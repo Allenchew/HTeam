625549a266cc632c715dcd4876acb1775ef240bb
nakano0093 <k165024@kccollege.ac.jp>
2018-12-18 20:25:21 +0900

    t_nakano MapScripte_Update

---
+++ Assets/t_nakano/nakano_Scripte/MapControl.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class MapControl : MonoBehaviour {
+
+	public TextAsset textAsset;
+
+	public float polygon=6; //角の数
+
+	public float distance = 0.5f; //壁を立てる距離
+
+	public GameObject wall;
+
+	public List<int> stageDate = new List<int>();
+
+	public enum STAGE_KIND{
+		none,brake
+	};
+
+	// Use this for initialization
+	void Start () {
+
+		CreateStage ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void CreateStage(){
+
+		string stageTextData = textAsset.text;
+
+		int i = 0;
+		int j = 1;
+
+		foreach(char c in stageTextData){
+
+			//GameObject obj = null;
+
+			//Debug.Log (c);
+
+			if(c == 'w'){
+				stageDate.Add (1);
+				int n = i / j;
+
+				float _angle = 360.0f / polygon;
+
+				float _rad = _angle*Mathf.Deg2Rad;
+
+				//Debug.Log (angle);
+
+				float x = Mathf.Cos (_rad * n) * distance * j;
+				float z = Mathf.Sin (_rad * n) * distance * j;
+
+				float i_angle = n * _angle;
+
+				GameObject obj = GameObject.Instantiate (wall, new Vector3 (x, 0.0f, z),Quaternion.Euler(new Vector3(0.0f,-i_angle,0.0f))/*new Quaternion(0.0f,2.0f,0.0f,1.0f)*/);
+
+				float side = (j - 1) * 0.5f;
+
+				float side2 = i % j * 1.5f;
+
+				obj.transform.localPosition += new Vector3 (side2, 0.0f, 0.0f);
+
+			}else if(c == '\n'){
+				j++;
+				Debug.Log (j);
+				continue;
+			}else if(c == ' '){
+				stageDate.Add (0);
+			}
+			i++;
+			//Debug.Log (i);
+			//Debug.Log (j);
+		}
+		Debug.Log (stageDate);
+
+	}
+}
---
+++ Assets/t_nakano/nakano_Scripte/MapCreate.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MapCreate : MonoBehaviour {
+
+	public int hierarchy = 3 ; // 階層
+	public int corner = 6; //　角の数
+
+	public GameObject wall;
+
+	enum MAP_STATE{ none,stand };
+
+	struct MapData{
+		Vector3 pos;
+		Vector3 rot;
+		MAP_STATE state;
+	};
+	private List<MapData> _mapData = new List<MapData> ();
+
+	public TextAsset stageText;
+	//private TextAsset stageText;
+
+	// Use this for initialization
+	void Start () {
+
+		CreateMap ("stage_01");
+
+	}
+	void CreateMap(string stageName){
+		
+		string stageTextData = stageText.text;
+
+
+
+		foreach (char c in stageTextData) {
+
+			if (c == '\n') {
+				continue;
+			}
+
+			float chipAngle = 360.0f / corner;
+			float chipRad = chipAngle*Mathf.Deg2Rad;
+
+
+
+
+			if (c == 'w') {
+				//Instantiate (obj, new Vector3 (0.0f, 0.0f, 0.0f), Quaternion.Euler (new Vector3 (0.0f, 0.0f, 0.0f)));
+			} else if (c == ' ') {
+
+			} 
+		}
+	}
+}
