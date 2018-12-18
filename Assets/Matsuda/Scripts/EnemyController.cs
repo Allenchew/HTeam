fd541ee18da7ba849182b5adf49d463694686082
matsudakk <melonwater250@gmail.com>
2018-12-18 10:08:50 +0900

    移動の処理を関数化(EnemyMove) 当たったオブジェクトのタグをswitch文で判定(Tower→動きが止まる、Wall→デストロイする)

--- Assets/Matsuda/Scripts/EnemyController.cs
+++ Assets/Matsuda/Scripts/EnemyController.cs
@@ -9,25 +9,31 @@ public class EnemyController : MonoBehaviour {
 
     Rigidbody _rb;
 
-    #region//
+    #region//タワーに向かって移動する処理
+    //移動するかしないか
+    [SerializeField]
+    bool _move = true;
+
     //移動開始時のポジション
     Vector3 _startPos;
 
     //今の時間
     float _timeCount = 0;
 
-    [Tooltip("向かう先")]
-    public GameObject Tower;
+    [Tooltip("向かう先"),SerializeField]
+    GameObject Tower;
 
     //向かう先のポジション
     Vector3 _towerPos;
 
-    [Tooltip("移動速度")]
-    public float EnemySpeed;
+    [Tooltip("移動速度"),SerializeField]
+    float EnemySpeed;
+    #endregion
 
+    [Tooltip("壁"),SerializeField]
+    GameObject Wall;
 
-
-	void Start () {
+    void Start () {
         _rb = GetComponent<Rigidbody>();
         _startPos = transform.position;
 
@@ -36,7 +42,18 @@ public class EnemyController : MonoBehaviour {
 	}
 
     void Update() {
-        #region//移動処理
+        gameObject.transform.LookAt(Tower.transform);//Towerの方向に向く
+
+        if (_move){
+            EnemyMove();//移動
+        }
+    }
+
+    /// <summary>
+    /// //移動処理
+    /// </summary>
+    void EnemyMove()
+    {
         _timeCount += Time.deltaTime;
 
         _rb.MovePosition(
@@ -44,15 +61,27 @@ public class EnemyController : MonoBehaviour {
             Mathf.Lerp(_startPos.x, _towerPos.x, _timeCount * EnemySpeed),//X軸
             _startPos.y,//Y軸
             Mathf.Lerp(_startPos.z, _towerPos.z, _timeCount * EnemySpeed)));//Z軸
-        #endregion
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
-        if(collision.gameObject.tag == Tower.tag)
-        {
-            Destroy(gameObject);
+
+        switch(collision.gameObject.tag){
+
+            //Wallのタグ
+            case "Wall":
+                Destroy(gameObject);
+                break;
+
+            //Towerのタグ
+            case "Tower":
+                //止まる
+                _move = false;
+                break;
+
+            default:
+                break;
         }
     }
 
