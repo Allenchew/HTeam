f52b870bc1cecbb3648e15e1322bea340f2b9007
matsudakk <melonwater250@gmail.com>
2018-12-19 13:54:49 +0900

    壁に沿って移動する処理(1方向限定) 謎の0.5不足(移動量)

--- Assets/Matsuda/Scripts/EnemyController.cs
+++ Assets/Matsuda/Scripts/EnemyController.cs
@@ -11,11 +11,9 @@ public class EnemyController : MonoBehaviour
     Rigidbody _rb;
 
     #region//タワーに向かって移動する処理
-    //侵攻しているならtrue
+    //動いているならtrue
     [SerializeField]
-    bool _move = true;
-
-    bool sidemove = false;
+    bool _move = false;
 
     [Tooltip("向かう先"), SerializeField]
     GameObject _tower;
@@ -29,15 +27,21 @@ public class EnemyController : MonoBehaviour
 
 
     #region//壁を避ける処理
-    [Tooltip("壁"), SerializeField]
-    GameObject _wall;
-
     //当たっている壁
     GameObject _hitWall;
 
-    [SerializeField]
+    [Tooltip("レイの長さ"), SerializeField]
     float _rayLength;
-    
+
+    [Tooltip("遅延時間"), SerializeField]
+    float _delayTime;
+
+    [Tooltip("壁に当たっているか(F)"), SerializeField]
+    bool _wallF;
+    [Tooltip("壁に当たっているか(R)"), SerializeField]
+    bool _wallR;
+    [Tooltip("壁に当たっているか(L)"), SerializeField]
+    bool _wallL;
     #endregion
 
 
@@ -47,12 +51,24 @@ public class EnemyController : MonoBehaviour
 
         _towerPos = _tower.transform.position;//Towerのポジション取得
 
-        transform.LookAt(_towerPos);//Towerの方向に向く
-        StartCoroutine(EnemyMove());//タワーに向かって移動
+        //Towerの方向に向く
+        transform.LookAt(_towerPos);
+
+        //タワーに向かって移動
+        StartCoroutine(EnemyMove());
     }
 
 
     void Update()
+    {
+        if(!_move)
+        {
+        StartCoroutine(EnemyMove());
+        }
+    }
+
+
+    void WallCheck()
     {
         //レイの準備
         Ray _rayForward;
@@ -62,13 +78,6 @@ public class EnemyController : MonoBehaviour
         RaycastHit _hitRight;
         RaycastHit _hitLeft;
 
-
-        if (_move == false)
-        {
-            _move = true;
-            StartCoroutine(EnemyMove());
-        }
-
         //タワーの方向にレイを飛ばす
         _rayForward = new Ray(transform.position, transform.forward);//new Ray(始め,方向)
         _rayRight = new Ray(transform.position, transform.right);
@@ -77,57 +86,44 @@ public class EnemyController : MonoBehaviour
         Debug.DrawRay(_rayRight.origin, _rayRight.direction, Color.red, 3.0f);
         Debug.DrawRay(_rayLeft.origin, _rayLeft.direction, Color.red, 3.0f);
 
-
-
         if (Physics.Raycast(_rayForward, out _hitForward, _rayLength))
         {//Physics.Raycast(判定するレイ,ヒットしたもの,長さ))
-            WallCheck(_rayForward, _hitForward);
-        }
-
-        if (Physics.Raycast(_rayRight, out _hitRight, _rayLength+0.2f))
-        {
-            WallCheck(_rayRight, _hitRight);
-        }
-
-        if (Physics.Raycast(_rayLeft, out _hitLeft, _rayLength+0.2f))
-        {
-            WallCheck(_rayLeft, _hitLeft);
-        }
-
-    }
-
-
-    void WallCheck(Ray _ray, RaycastHit _hit)
-    {
-        if (_hit.transform.tag == "Wall")
-        {//レイがWallのタグに当たっている場合
-
-            if (_hitWall == null)
-            {
-                //当たった壁の情報を保持
-                _hitWall = _hit.collider.gameObject;
-
-                //止まる
-                _move = false;
-
-                //向きを変える
-                transform.Rotate(new Vector3(0, 90, 0));
-                
-            }
-
-            if (_hit.collider.name != _hitWall.name)
-            {
-                //当たった壁の情報を保持
-                _hitWall = _hit.collider.gameObject;
-
-                //止まる
-                _move = false;
-
-                //向きを変える
-                transform.Rotate(new Vector3(0, -60, 0));
+            if (_hitForward.transform.tag == "Wall")
+            {//レイがWallのタグに当たっている場合
+                _hitWall = _hitForward.collider.gameObject;
+                _wallF = true;
             }
         }
+        else
+        {
+            _wallF = false;
+        }
+
+        if (Physics.Raycast(_rayRight, out _hitRight, _rayLength))
+        {//Physics.Raycast(判定するレイ,ヒットしたもの,長さ))
+            if (_hitRight.transform.tag == "Wall")
+            {//レイがWallのタグに当たっている場合
+                _wallR = true;
+            }
+        }
+        else
+        {
+            _wallR = false;
+        }
+
+        if (Physics.Raycast(_rayLeft, out _hitLeft, _rayLength))
+        {//Physics.Raycast(判定するレイ,ヒットしたもの,長さ))
+            if (_hitLeft.transform.tag == "Wall")
+            {//レイがWallのタグに当たっている場合
+                _wallL = true;
+            }
+        }
+        else
+        {
+            _wallL = false;
+        }
     }
+
     ///// <summary>
     ///// 移動処理
     ///// </summary>
@@ -149,12 +145,62 @@ public class EnemyController : MonoBehaviour
     /// </summary>
     IEnumerator EnemyMove()
     {
-        while (_move)
+        _move = true;
+
+        while (!_wallF)
         {
-            _rb.MovePosition(Vector3.Lerp(transform.position,transform.position + transform.forward,Time.deltaTime*_enemySpeed));
+            _rb.MovePosition(Vector3.Lerp(transform.position, transform.position + transform.forward, Time.deltaTime * _enemySpeed));
+            WallCheck();
 
             yield return null;
         }
+
+        //向きを変える
+        transform.Rotate(new Vector3(0, 90, 0));
+
+        float a = (_hitWall.transform.localScale.x / 2 
+            + (_rayLength * Mathf.Tan(Mathf.PI / 6)) + 0.4f) 
+            / _enemySpeed;
+
+        Debug.Log(_rayLength * Mathf.Tan(Mathf.PI / 6));
+        _delayTime = a;
+
+        WallCheck();
+        
+
+        while (_wallL)
+        {
+            float _timeCount = 0;
+
+            while (_delayTime > _timeCount)
+            {
+                _timeCount += Time.deltaTime;
+                _rb.MovePosition(Vector3.Lerp(transform.position, transform.position + transform.forward, Time.deltaTime * _enemySpeed));
+                
+                yield return null;
+            }
+
+            //向きを変える
+            transform.Rotate(new Vector3(0, -60, 0));
+
+            _timeCount = 0;
+
+            while (_delayTime > _timeCount)
+            {
+                _timeCount += Time.deltaTime;
+
+                _rb.MovePosition(Vector3.Lerp(transform.position, transform.position + transform.forward, Time.deltaTime * _enemySpeed));
+
+                yield return null;
+            }
+
+            WallCheck();
+        }
+
+        //Towerの方向に向く
+        transform.LookAt(_towerPos);
+
+        _move = false;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -169,8 +215,6 @@ public class EnemyController : MonoBehaviour
 
             //Towerのタグ
             case "Tower":
-                //止まる
-                _move = false;
 
                 //
                 //攻撃のアニメーション
